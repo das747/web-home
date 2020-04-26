570442e86c9eae9cf4eec7841f8b12fb9e033b1a
das747 <40293014+das747@users.noreply.github.com>
2020-04-26 23:06:10 +0700

    'testing autodeploy'

--- autodeploy.py
+++ autodeploy.py
@@ -2,16 +2,16 @@ from flask import Blueprint, request
 import hmac
 import os
 
-SUPER_SECRET_KEY = open('deploy_secret_key.txt', mode='r').read().strip()
+SUPER_SECRET_KEY = bytes(open('deploy_secret_key.txt', mode='r').read(), 'utf8')
 blueprint = Blueprint('autodeployment', __name__, template_folder='templates')
 
 
 @blueprint.route('/deploy', methods=['POST'])
 def autodeploy():
     hex_digest = request.headers['X-Hub-Signature']
-    payload = request.json['payload']
-    hasher = hmac.new(SUPER_SECRET_KEY, payload, 'sha1')
+    payload = request.json
+    hasher = hmac.new(SUPER_SECRET_KEY, request.data, 'sha1')
     if (hmac.compare_digest(hex_digest, hasher.hexdigest())
-            and payload['refs'].split('/')[:-1] == 'deploy'):
+            and payload['refs'].split('/', 2)[2] == 'deploy'):
         os.system('git pull && sudo systemctl restart webhome')
 
