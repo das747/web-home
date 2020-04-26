c5e98493d00e11872cf02ccae70891968f2cc16e
das747 <40293014+das747@users.noreply.github.com>
2020-04-26 21:58:07 +0700

    'testing autodeploy'

--- autodeploy.py
+++ autodeploy.py
@@ -9,7 +9,7 @@ blueprint = Blueprint('autodeployment', __name__, template_folder='templates')
 @blueprint.route('/deploy', methods=['POST'])
 def autodeploy():
     hex_digest = request.headers['X-Hub-Signature']
-    payload = request.json()['payload']
+    payload = request.json['payload']
     hasher = hmac.new(SUPER_SECRET_KEY, payload, 'sha1')
     if (hmac.compare_digest(hex_digest, hasher.hexdigest())
             and payload['refs'].split('/')[:-1] == 'deploy'):
